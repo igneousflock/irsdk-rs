542dffaf3a58391a6e177bf72b1a46008e7f395e
Alex van de Sandt <alex@avandesa.dev>
2025-12-02 08:22:54 -0600

    Dump session info and headers more cleanly

--- examples/dump_file.rs
+++ examples/dump_file.rs
@@ -4,7 +4,8 @@ fn main() {
     let file_name = std::env::args()
         .nth(1)
         .expect("Usage: dump_file <PATH_TO_IBT>");
-    let file = IbtFile::from_file(&file_name).unwrap();
+    let file = IbtFile::from_file(&file_name).expect("could not open IBT file");
 
-    dbg!(&file.header, &file.disk_sub_header, &file.var_headers);
+    println!("{:#?}", &file.header);
+    println!("{:#?}", &file.disk_sub_header);
 }
--- examples/dump_session_string.rs
+++ examples/dump_session_string.rs
@@ -1,11 +1,18 @@
-
 use irsdk::IbtFile;
+use saphyr::YamlEmitter;
 
 fn main() {
     let file_name = std::env::args()
         .nth(1)
         .expect("Usage: dump_file <PATH_TO_IBT>");
-    let file = IbtFile::from_file(&file_name).unwrap();
+    let file = IbtFile::from_file(&file_name).expect("could not open IBT file");
 
-    dbg!(file.session_data().unwrap());
+    let session_data = file.session_data().expect("could not parse session data");
+    let mut output = String::new();
+    let mut emitter = YamlEmitter::new(&mut output);
+    emitter.compact(true);
+    emitter
+        .dump(&saphyr::Yaml::from(&session_data))
+        .expect("could not serialize session data");
+    println!("{output}");
 }
